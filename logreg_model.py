24dcdf4df5e7aa4ff8561ae195b458e5e7af7299
Randy Tang <rt378@cornell.edu>
2021-12-01 19:15:15 -0500

    deleted some missing fields

--- logreg_model.py
+++ logreg_model.py
@@ -7,8 +7,6 @@ from sklearn.model_selection import GridSearchCV,train_test_split
 from sklearn import metrics
 from tqdm import tqdm
 
-from datetime import datetime
-
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
@@ -17,8 +15,7 @@ from sklearn.model_selection import KFold
 
 import sys
 
-
-data_path = "games.csv"
+data_path = "2018_last5_fixed.csv"
 
 def gen_pipeline(use_pca=True, pca_components=10, scale_data=True):
     pipeline_components = []
@@ -59,9 +56,9 @@ principalDf = pd.DataFrame(data = principalComponents)
 
 
 
-X_train, X_test, y_train, y_test = train_test_split(principalDf, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(principalDf, y, test_size=0.25, random_state=42)
 
-clf = LogisticRegression(max_iter=1000)
+clf = LogisticRegression(max_iter=100)
 
 clf.fit(X_train,y_train)
 y_pred = clf.predict(X_test)
